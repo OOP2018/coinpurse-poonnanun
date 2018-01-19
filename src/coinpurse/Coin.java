c316fb16403ac541b37214a7ddb87b0deb1b0904
poonnanun <poonnanun1@gmail.com>
2018-01-19 14:17:54 +0700

    complete coin class

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -1,6 +1,6 @@
 package coinpurse;
 
-public class Coin {
+public class Coin implements Comparable<Coin>{
 
 	private double value;
 	private String currency;
@@ -17,17 +17,41 @@ public class Coin {
 	public String getCurrency(){
 		return this.currency;
 	}
+
 	@Override
-	public boolean equals(Object arg){
-		return false;
+	public boolean equals(Object obj) {
+		if (this == obj){
+			return true;
+		}
+		if (obj == null){
+			return false;
+		}
+		Coin other = (Coin) obj;
+		if (!currency.equalsIgnoreCase(other.currency)){
+			return false;
+		}
+			
+		if (value != value){
+			return false;
+		}
+		return true;
 	}
 	
 	public int compareTo(Coin coin){
-		return 0;
+		if(this.value < coin.getValue()){
+			return -1;
+		}
+		else if(this.value == coin.getValue()){
+			return 0;
+		}
+		else{
+			return 1;
+		}
+		
 	}
 	
 	@Override
 	public String toString(){
-		return null;
+		return value+"="+currency;
 	}
 }
